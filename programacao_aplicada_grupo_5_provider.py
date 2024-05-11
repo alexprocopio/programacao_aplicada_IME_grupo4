54815c2b22b95f2ccecdb9e5a3e562c48cca607e
alexprocopio <92341170+alexprocopio@users.noreply.github.com>
2024-05-11 20:38:25 -0300

    ...

--- programacao_aplicada_grupo_5_provider.py
+++ programacao_aplicada_grupo_5_provider.py
@@ -31,8 +31,8 @@ __copyright__ = '(C) 2024 by Grupo 5'
 __revision__ = '$Format:%H$'
 
 from qgis.core import QgsProcessingProvider
-from .algorithms.Projeto1.solucao import Projeto1Solucao
-from .algorithms.Projeto1.solucao_complementar import Projeto1SolucaoComplementar
+from .algorithms.Projeto2.solucao import Projeto2Solucao
+from .algorithms.Projeto2.solucao_complementar import Projeto2SolucaoComplementar
 
 
 class ProgramacaoAplicadaGrupo5Provider(QgsProcessingProvider):
@@ -54,8 +54,8 @@ class ProgramacaoAplicadaGrupo5Provider(QgsProcessingProvider):
         """
         Loads all algorithms belonging to this provider.
         """
-        self.addAlgorithm(Projeto1Solucao())
-        self.addAlgorithm(Projeto1SolucaoComplementar())
+        self.addAlgorithm(Projeto2Solucao())
+        self.addAlgorithm(Projeto2SolucaoComplementar())
 
         # add additional algorithms here
         # self.addAlgorithm(MyOtherAlgorithm())
